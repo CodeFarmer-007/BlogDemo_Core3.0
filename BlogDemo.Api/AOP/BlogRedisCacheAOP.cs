f519ce505a13ff41145d555913fb02c3c1021e9c
赵军 <229318442@qq.com>
2020-03-02 11:33:35 +0800

    RedisCacheAOP，【Redis缓存AOP】

---
+++ BlogDemo.Api/AOP/BlogRedisCacheAOP.cs
@@ -0,0 +1,107 @@
+﻿using BlogDemo.Core.Common.Attribute;
+using BlogDemo.Core.Common.Redis;
+using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogDemo.Api.AOP
+{
+    /// <summary>
+    /// 面向切面的缓存使用  【Redis缓存截器】
+    /// </summary>
+    public class BlogRedisCacheAOP : CacheAOPbase
+    {
+        //通过注入的方式，把缓存操作接口通过构造函数注入
+        private readonly IRedisCacheManager _cache;
+        public BlogRedisCacheAOP(IRedisCacheManager cache)
+        {
+            _cache = cache;
+        }
+
+        //Intercept方法是拦截的关键所在，也是IInterceptor接口中的唯一定义
+        public override void Intercept(IInvocation invocation)
+        {
+            var method = invocation.MethodInvocationTarget ?? invocation.Method;
+
+            //对当前方法的特性验证
+            var qCachingAttribute = method.GetCustomAttributes(true).FirstOrDefault(x => x.GetType() == typeof(CachingAttribute)) as CachingAttribute;
+
+            if (qCachingAttribute != null)
+            {
+                //获取自定义缓存键
+                var cacheKey = CustomCacheKey(invocation);
+                //注意 string 类型，方法GetValue
+                var cacheValue = _cache.GetValue(cacheKey);
+
+                //已有缓存数据，则获取
+                if (cacheValue != null)
+                {
+                    //将当前获取到的缓存值，赋值给当前执行方法
+                    var type = invocation.Method.ReturnType;
+                    var resultTypes = type.GenericTypeArguments;
+                    if (type.FullName == "System.Void")
+                    {
+                        return;
+                    }
+
+                    object response;
+                    if (typeof(Task).IsAssignableFrom(type))
+                    {
+                        //返回Task<T>
+                        if (resultTypes.Any())
+                        {
+                            var resultType = resultTypes.FirstOrDefault();
+                            dynamic temp = Newtonsoft.Json.JsonConvert.DeserializeObject(cacheValue, resultType);
+                            response = Task.FromResult(temp);
+                        }
+                        else
+                        {
+                            response = Task.Yield();
+                        }
+                    }
+                    else
+                    {
+                        response = Convert.ChangeType(_cache.Get<object>(cacheKey), type);
+                    }
+
+                    invocation.ReturnValue = response;
+                    return;
+                }
+
+                //去执行当前的方法
+                invocation.Proceed();
+
+                //没有Key添加Key，存入缓存
+                if (!string.IsNullOrWhiteSpace(cacheKey))
+                {
+                    object response;
+
+                    var type = invocation.Method.ReturnType;
+                    if (typeof(Task).IsAssignableFrom(type))
+                    {
+                        var resultProperty = type.GetProperty("Result");
+                        response = resultProperty.GetValue(invocation.ReturnValue);
+                    }
+                    else
+                    {
+                        response = invocation.ReturnValue;
+                    }
+
+                    if (response == null)
+                    {
+                        response = string.Empty;
+                    }
+
+                    //执行Key赋值
+                    _cache.Set(cacheKey, response, TimeSpan.FromMinutes(qCachingAttribute.AbsoluteExpiration));
+                }
+            }
+            else
+            {
+                invocation.Proceed();//直接执行被拦截方法
+            }
+        }
+    }
+}
