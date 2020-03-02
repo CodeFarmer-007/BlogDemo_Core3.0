3c19b3223db069fb5c6e715de3f00219c0259d5f
赵军 <229318442@qq.com>
2020-03-02 15:10:37 +0800

    1

--- BlogDemo.Api/AOP/BlogCacheAOP.cs
+++ BlogDemo.Api/AOP/BlogCacheAOP.cs
@@ -15,38 +15,32 @@ namespace BlogDemo.Api.AOP
     {
         //通过注入的方式，把缓存操作接口通过构造函数注入
         private readonly ICaching _cache;
-
         public BlogCacheAOP(ICaching cache)
         {
             _cache = cache;
         }
 
-        //实现继承的抽象类  Intercept方法是拦截的关键所在，也是IInterceptor接口中的唯一定义
+        //Intercept方法是拦截的关键所在，也是IInterceptor接口中的唯一定义
         public override void Intercept(IInvocation invocation)
         {
-            //添加判断条件--有属性、特性的才进行缓存操作
             var method = invocation.MethodInvocationTarget ?? invocation.Method;
-
             //对当前方法的特性验证
             var qCachingAttribute = method.GetCustomAttributes(true).FirstOrDefault(x => x.GetType() == typeof(CachingAttribute)) as CachingAttribute;
-
             //只有那些指定的才可以被缓存，需要验证
             if (qCachingAttribute != null)
             {
                 //获取自定义缓存键
                 var cacheKey = CustomCacheKey(invocation);
-
-                //根据Key获取值
-                var cachValue = _cache.Get(cacheKey);
-
-                if (cachValue != null)
+                //根据key获取相应的缓存值
+                var cacheValue = _cache.Get(cacheKey);
+                if (cacheValue != null)
                 {
                     //将当前获取到的缓存值，赋值给当前执行方法
-                    invocation.ReturnValue = cachValue;
+                    invocation.ReturnValue = cacheValue;
                     return;
                 }
 
-                //去执行当前方法
+                //去执行当前的方法
                 invocation.Proceed();
 
                 //存入缓存
@@ -54,6 +48,10 @@ namespace BlogDemo.Api.AOP
                 {
                     _cache.Set(cacheKey, invocation.ReturnValue);
                 }
+            }
+            else
+            {
+                invocation.Proceed();//直接执行被拦截方法
             }
 
         }
