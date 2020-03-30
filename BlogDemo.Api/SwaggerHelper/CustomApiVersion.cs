fc486ed4262eab8906d43d7e36164acd38ff0825
赵军 <229318442@qq.com>
2020-03-30 10:34:44 +0800

    添加Swagger版本控制

---
+++ BlogDemo.Api/SwaggerHelper/CustomApiVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogDemo.Api.SwaggerHelper
+{
+    /// <summary>
+    /// 自定义Api版本
+    /// </summary>
+    public class CustomApiVersion
+    {
+        public enum ApiVersions
+        {
+            /// <summary>
+            /// v1 版本
+            /// </summary>
+            v1 = 1,
+            /// <summary>
+            /// v2 版本
+            /// </summary>
+            v2 = 2
+        }
+    }
+}
